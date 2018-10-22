2814d596321bad7a51c9ff26aee5fd556fcb29ea
Everton Vinicius Prates <everton.prates@castinformatica.onmicrosoft.com>
2018-10-22 14:49:40 -0300

    Adiciona versão com rotas funcionando

---
+++ src/app/components/index/index.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IndexRoutingModule } from './index-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IndexRoutingModule
+  ],
+  declarations: []
+})
+export class IndexModule { }
